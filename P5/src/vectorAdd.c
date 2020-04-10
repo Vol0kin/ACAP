b56ec09aeeda17373bb3defacb1eab277e4c5215
Vladislav Nikolov <vladis890@gmail.com>
2020-04-10 21:27:29 +0200

    Added sequential version

--- P5/src/vectorAdd.c
+++ P5/src/vectorAdd.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <omp.h>
 
 void allocate_array(float **array, int N)
 {
@@ -52,6 +54,16 @@ void read_file(char* file, float** array, int* N)
     }
 }
 
+void add_vectors(float *A, float *B, float *C, int N)
+{
+    int i;
+
+    for (i = 0; i < N; i++)
+    {
+        C[i] = pow(pow(log(5*A[i]*100*B[i] + 7*A[i]) / 0.33, 3), 7);
+    }
+}
+
 int main(int argc, char* argv[])
 {
     if (argc != 3)
@@ -60,15 +72,26 @@ int main(int argc, char* argv[])
         printf("Usage: %s [input data 1] [input data 2]\n", argv[0]);
         exit(EXIT_FAILURE);
     }
-    
-    // Read files and allocate arrays
+
+    double t1, t_time;
     float *A, *B, *C;
     int N;
 
+    // Read files and allocate arrays
     read_file(argv[1], &A, &N);
     read_file(argv[2], &B, &N);
     allocate_array(&C, N);
 
+    // Add vectors
+    t1 = omp_get_wtime();
+
+    add_vectors(A, B, C, N);
+
+    t_time = omp_get_wtime() - t1;
+
+    // Show some values
+    printf("C[0] = %f\nC[1] = %f\nC[%d] = %f\nC[%d] = %f\n", C[0], C[1], N-2, C[N-2], N-1, C[N-1]);
+    printf("Total time: %f\n", t_time);
 
     // Free memory
     free(A);
